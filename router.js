ae8d69cf69fcf57477841f780a4c69b3a6b8d775
BarryMurphy1992 <b.murphy50@nuigalway.ie>
2017-03-30 11:26:52 +0100

    Add files via upload

---
+++ router.js
@@ -0,0 +1,15 @@
+Router.route('/', {
+  template: 'home'
+});
+Router.route('/aboutUs');
+Router.route('/email');
+Router.route('/findUs');
+Router.route('/location');
+Router.route('/login');
+Router.route('/searchForm');
+Router.route('/collegeInfo');
+Router.route('/map');
+Router.route('/message');
+
+
+
